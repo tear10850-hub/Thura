36bdab5fb486331c17e63c298f8b3229b1d57d48
tear10850-hub <tear10850@gmail.com>
2026-08-06 05:08:22 +0630

    Remove PORT variable assignment
    Removed PORT variable assignment from bot.js.

--- bot.js
+++ bot.js
@@ -27,7 +27,6 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const MONGO_URI = process.env.MONGO_URI;
 const SECOND_MONGO_URI = process.env.SECOND_MONGO_URI || process.env.MONGO_URI;
 const OWNER_ID = Number(process.env.OWNER_ID);
-const PORT = process.env.PORT || 3000;
 
 // Env Variables မပြည့်စုံပါက Error ထုတ်ပြီး အေးဆေး ရပ်တန့်မည်
 if (!BOT_TOKEN || !MONGO_URI || !OWNER_ID) {
