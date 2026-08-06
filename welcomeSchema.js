43d478c159f3cd23041f5655efc0940a2e09a1eb
tear10850-hub <tear10850@gmail.com>
2026-08-07 01:29:05 +0630

    Delete welcomeSchema.js

--- welcomeSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const WelcomeSchema = new mongoose.Schema({
-  chatId: { type: Number, required: true, unique: true },
-  welcomeVideoId: { type: String, default: null },
-  updatedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Welcome', WelcomeSchema);
